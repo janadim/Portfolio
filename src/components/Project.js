dba50dacca53803a59d9ba797a9e09e7ad5dce9f
JanaDTitarchuk <Jana.d.titarchuk@gmail.com>
2024-01-10 11:23:57 +0100

    run docs

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,24 +2,26 @@ import React from "react";
 
 function Project(props) {
   return (
-    <div className="proyectos__rectangle">
-      <h3 className="proyectos__name">{props.name}</h3>
-      <p className="proyectos__description">{props.description}</p>
+    <a className="proyectos__enlace" href={props.web} target="_blank">
+      <div className="proyectos__rectangle ">
+        <h3 className="proyectos__name">{props.name}</h3>
+        <p className="proyectos__description">{props.description}</p>
 
-      <ul className="proyectos__list">
-        <li className="proyectos__list--link">
-          <a href={props.repo} target="_blank">
-            <i className="proyectos__enlace--icon fas fa-code"></i>
-          </a>
-        </li>
-        <li className="proyectos__list--link">
-          <a className="proyectos__enlace" href={props.web} target="_blank">
-            <i className="proyectos__enlace--icon fas fa-laptop"></i>
-          </a>
-        </li>
-      </ul>
-      <img className="proyectos__image" src={props.img} alt={props.name} />
-    </div>
+        <ul className="proyectos__list">
+          <li className="proyectos__list--link">
+            <a href={props.repo} target="_blank">
+              <i className="proyectos__enlace--icon fas fa-code"></i>
+            </a>
+          </li>
+          <li className="proyectos__list--link">
+            <a className="proyectos__enlace" href={props.web} target="_blank">
+              <i className="proyectos__enlace--icon fas fa-laptop"></i>
+            </a>
+          </li>
+        </ul>
+        <img className="proyectos__image" src={props.img} alt={props.name} />
+      </div>
+    </a>
   );
 }
 export default Project;
